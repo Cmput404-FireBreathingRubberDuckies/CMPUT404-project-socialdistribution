d9a93ca2f6a8b45dbf0f5d6efe42f4737f5dde5e
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-02-06 11:19:37 -0700

    Update database settings

--- socialp2p/settings.py
+++ socialp2p/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'socialp2p.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'd4ihuk572s4c8l',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'jceokqmtcmeild',
+            'PASSWORD': 'kGV18E4FOPI3S5l1e7gsk8feoV',
+            'HOST': 'ec2-54-235-152-114.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                      # Set to empty string for default.
+        }
 }
 
 
