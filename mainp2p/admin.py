b7430f3d2c11124f3cf82d1554180fbeb5e3e2ad
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-02-06 15:34:04 -0700

    update database model

--- mainp2p/admin.py
+++ mainp2p/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from mainp2p.models import Post
+from mainp2p.models import Post, Author
 
+admin.site.register(Author)
 admin.site.register(Post)
--- mainp2p/models.py
+++ mainp2p/models.py
@@ -5,8 +5,14 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Post(models.Model):
-	uid = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	markdown = models.BooleanField(default=False)
 	content = models.TextField()
-	image = models.ImageField(upload_to="images/")
+	image = models.ImageField(upload_to="images/post")
 	visibility = models.CharField(max_length=30)
+	user_can_view = models.ForeignKey(User, related_name='+', blank=True)
+
+class Author(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	host = models.CharField(max_length=100, default="local")
+	photo = models.ImageField(upload_to="images/profile", blank=True)
