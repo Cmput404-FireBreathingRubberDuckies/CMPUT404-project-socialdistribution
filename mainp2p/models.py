b361986916bb23bae74374d21dbdc7444bbda1c6
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-02-06 16:31:57 -0700

    update database

--- mainp2p/models.py
+++ mainp2p/models.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
 class Post(models.Model):
@@ -13,6 +11,7 @@ class Post(models.Model):
 	visibility = models.CharField(max_length=30)
 	user_can_view = models.ForeignKey(User, related_name='+', blank=True)
 
-class Author(AbstractBaseUser):
-	host = models.CharField(max_length=100, default="local")
-	photo = models.ImageField(upload_to="images/profile", blank=True)
+class Author(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	host = models.CharField(max_length=30)
+	photo = models.ImageField(upload_to="images/profile", null=True)
