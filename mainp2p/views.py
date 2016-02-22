36e9d3cd05774f9f77590b66e0cf9b05cf59a9be
Brandon Cheung <bwcheung@ualberta.ca>
2016-02-22 04:27:58 +0000

    more playing around

--- mainp2p/views.py
+++ mainp2p/views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
-
-from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext
+from django.shortcuts import render_to_response, render
 from django.contrib.auth.models import User
 from mainp2p.models import Author
 
@@ -13,9 +13,11 @@ def main(request):
 	Username = request.GET["UserName"]
     	Password = request.GET["Password"]
 	if User.objects.filter(username=Username).exists():
-		return render(request, 'mainp2p/login.html')
+		 return render(request, 'mainp2p/login.html')
 	else:
 	    	user = User.objects.create_user(username=Username,
 						 email="",
 						password=Password)
-	return render(request, 'mainp2p/index.html')
+		name = Author.objects.all()
+		context = {"name": name, "Username" : Username}
+		return render_to_response('mainp2p/index.html', context, context_instance=RequestContext(request))
