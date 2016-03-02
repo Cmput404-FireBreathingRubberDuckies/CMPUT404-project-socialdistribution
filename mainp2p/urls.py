67b3b7f1c16df510d998a614c23317204ad03a54
ibarreto <iabarreto11@gmail.com>
2016-03-01 20:37:20 -0700

    add some checks. Basic sign up, not in a REST endpoint though

--- mainp2p/urls.py
+++ mainp2p/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^login', views.login_view, name='login'),
     url(r'^authenticate', views.authenticate_view, name='authenticate'),
     url(r'^logout', views.logout_view, name='logout'),
+    url(r'^signup', views.signup_view, name='signup'),
 ]
--- mainp2p/views.py
+++ mainp2p/views.py
@@ -9,36 +9,48 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('mainp2p:main'))
     return render(request, 'mainp2p/login.html')
 
 def authenticate_view(request):
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
-	if user is not None:
-		if user.is_active:
-			login(request, user)
-			return HttpResponseRedirect(reverse('mainp2p:main'))
-		else:
-			return
-	return HttpResponseRedirect(reverse('mainp2p:login'))
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('mainp2p:main'))
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponseRedirect(reverse('mainp2p:main'))
+        else:
+            return
+    return HttpResponseRedirect(reverse('mainp2p:login'))
 
 def logout_view(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('mainp2p:login'))
+    logout(request)
+    return HttpResponseRedirect(reverse('mainp2p:login'))
+
+def signup_view(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('mainp2p:main'))
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST['username'], None, request.POST['password'])
+
+        author = Author(user=user, host='localhost')
+        author.save()
+
+        a_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if a_user is not None:
+            if user.is_active:
+                login(request, a_user)
+                return HttpResponseRedirect(reverse('mainp2p:main'))
+            else:
+                return HttpResponseRedirect(reverse('mainp2p:signup'))
+        return HttpResponseRedirect(reverse('mainp2p:signup'))
+    else:
+        return render(request, 'mainp2p/signup.html')
 
 @login_required
 def main(request):
-	print request
-	return render(request, 'mainp2p/index.html')
-	# Username = request.GET["UserName"]
-    # Password = request.GET["Password"]
-	# if User.objects.filter(username=Username).exists():
-		 # return render(request, 'mainp2p/login.html')
-	# else:
-	    	# user = User.objects.create_user(username=Username,
-						 # email="",
-						# password=Password)
-		# name = Author.objects.all()
-		# context = {"name": name, "Username" : Username}
-		# return render_to_response('mainp2p/index.html', context, context_instance=RequestContext(request))
+    return render(request, 'mainp2p/index.html')
