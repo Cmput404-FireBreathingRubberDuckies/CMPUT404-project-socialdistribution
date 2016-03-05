b41b05e0acb15668e369b4a112acb7b5df0d8a32
Xiaohui Ma <xiaohui6@ualberta.ca>
2016-03-05 16:46:46 -0700

    testing cloudinary

--- settings/settings.py
+++ settings/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'socialp2p'
+    'socialp2p',
+    'cloudinary',
 ]
 
 MIDDLEWARE_CLASSES = [
