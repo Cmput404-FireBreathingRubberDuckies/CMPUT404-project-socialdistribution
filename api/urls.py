25691f73fa6f21d600a8f8d0b9bad8394b21acb0
ibarreto <iabarreto11@gmail.com>
2016-03-07 19:51:41 -0700

    some changes to the authors endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,17 @@ from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from api import views
+from api import views, models
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'^author/(?P<username>\w+)/$', views.Author_detail, 'author')
+
+# router = routers.DefaultRouter()
+# router.register(r'author', models.AuthorViewSet)
 
 urlpatterns = [
+    url(r'^authors/$', views.author_list),
+    url(r'^authors/(?P<author_uuid>[^/]+)/$', views.author_detail),
     #url(r'^author/(?P<pk>[0-9]+)/$', views.Author_detail),
-    url(r'^author/(?P<username>\w+)/$', views.Author_detail, name='detail'),
+    # url(r'^authors/(?P<user_uuid>\w+)/$', views.author_info, name='info'),
+    # url(r'^author/(?P<username>\w+)/$', views.Author_detail, name='detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,37 +1,52 @@
+import uuid
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from socialp2p.models import Author
 from api.serializations import AuthorSerializer
 from django.contrib.auth.models import User
+from rest_framework import viewsets, status
 
 class JSONResponse(HttpResponse):
-	def __init__(self, data, **kwargs):
-		content = JSONRenderer().render(data)
-		kwargs['content_type'] = 'application/json'
-		super(JSONResponse, self).__init__(content, **kwargs)
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
 
-def Author_detail(request, username):
-	try:
-		user = User.objects.get(username=username)
-		userid = user.author.id
-		author = Author.objects.get(id=userid)
-	except Author.DoesNotExist:
-		return HttpResponse(status=404)
+@api_view(['GET', 'POST'])
+def author_list(request):
+    if request.method == 'GET':
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
 
-	if request.method == 'GET':
-		serializer = AuthorSerializer(author)
-		return JSONResponse(serializer.data)
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = AuthorSerializer(author, data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JSONResponse(serializer.data)
-		return JSONResponse(serializer.errors, status=400)
-	elif request.method == 'DELETE':
-		author.delete()
-		return HttpResponse(status=204)
-# Create your views here.
+@api_view(['GET', 'PUT', 'DELETE'])
+def author_detail(request, author_uuid):
+    try:
+        author_uuid = uuid.UUID(author_uuid)
+    except Exception as e:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        author = Author.objects.get(uuid=author_uuid)
+    except Author.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = AuthorSerializer(author)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = AuthorSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
